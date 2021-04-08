e7a3eec1003b141be07744f431243b311418ef59
Arthur <19251998+arthur-ver@users.noreply.github.com>
2021-04-08 17:46:41 +0200

    Update fetchNFTs.tsx

--- pages/api/fetchNFTs.tsx
+++ pages/api/fetchNFTs.tsx
@@ -28,7 +28,6 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
         res.status(200).json({ nfts: tokens, count: count })
     }
-
     return query(cursor)
         .catch(e => res.status(502).json({error: e}))
 }
