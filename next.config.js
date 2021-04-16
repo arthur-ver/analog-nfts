5df6b2c1326ba29cd48eee2ec7515e50a69c4e15
Arthur <19251998+arthur-ver@users.noreply.github.com>
2021-04-16 16:12:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,5 @@ module.exports = {
     // Note: This is required to use the Zora ZDK on the client
     if (!isServer) config.node = { fs: 'empty' };
     return config;
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: `${process.env.NODE_ENV === 'development' ? '*' : process.env.ACCESS_ORIGIN}` },
-        ]
-      }
-    ]
   }
 };
